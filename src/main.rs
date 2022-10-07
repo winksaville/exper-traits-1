e6eee029e1f124be96ea90646cb269f918bbc5ac
Wink Saville <wink@saville.com>
2022-10-06 20:34:07 -0700

    refactor: Now it compiles
    This is less complete than the previous attempt but it does
    compile and run.

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,51 @@
-// Trait for Finite State Machines
-trait StateMachine<P> {
-    /// Return the current state
-    fn cur_state(&mut self) -> &mut Box<dyn State<Self, P> + 'static>;
-
-    fn set_state(&mut self, next_state: Box<dyn State<Self, P> + 'static>);
-
-    fn dispatch(&mut self, msg: &P)
-    where
-        Self: Sized,
-    {
-        self.cur_state().process(self, msg);
-    }
-}
-
 // Trait for States
-pub trait State<SM, P> {
-    fn process(&mut self, _sm: &mut SM, _msg: &P) {
-        println!("State: process");
-    }
+pub trait State<'g, SM, P> {
+    fn process(&self, sm: &'g mut SM, _msg: &'g P);
 }
 
 // Create a Protocal with two messages
-#[derive(Debug)]
-enum Protocol1 {
-    Msg1 { f1: i32 },
-}
-
 struct State1;
 
-impl State<MySm<Protocol1>, Protocol1> for State1 {
-    fn process(&mut self, sm: &mut MySm<Protocol1>, msg: &Protocol1) {
+impl<'g> State<'g, MySm<'g>, Protocol1> for State1 {
+    fn process(&self, sm: &'g mut MySm, msg: &'g Protocol1) {
         match *msg {
             Protocol1::Msg1 { f1 } => {
-                sm.data1 += 1 as f32;
+                sm.data1 += 1;
                 println!("State1: process sm.data1={} Msg1::f1={}", sm.data1, f1);
             }
         }
     }
 }
 
-struct MySm<Protocol1> {
-    current_state: Box<dyn State<Self, Protocol1> + 'static>,
-    data1: f32,
+#[derive(Debug)]
+enum Protocol1 {
+    Msg1 { f1: i32 },
 }
 
-impl<Protocol1> MySm<Protocol1> {
-    fn new(s: Box<dyn State<Self, Protocol1> + 'static>) -> Self {
+struct MySm<'g> {
+    current_state: &'g dyn State<'g, Self, Protocol1>,
+    data1: i32,
+}
+
+impl<'g> MySm<'g> {
+    fn new(s: &'g dyn State<'g, Self, Protocol1>) -> Self {
         MySm {
             current_state: s,
-            data1: 1.0,
+            data1: 1,
         }
     }
 }
 
-impl<Protocol1> StateMachine<Protocol1> for MySm<Protocol1> {
-    fn cur_state(&mut self) -> &mut Box<dyn State<Self, Protocol1> + 'static> {
-        println!("StateMachine::cur_state():+-");
-        &mut self.current_state
-    }
-
-    fn set_state(&mut self, next_state: Box<dyn State<Self, Protocol1> + 'static>) {
-        println!("StateMachine::set_state():+-");
-        self.current_state = next_state;
-    }
-}
-
 fn main() {
-    // Create state machine and initialize to State1
-    let mut mysm = Box::new(MySm::new(Box::new(State1 {})));
+    // Create State1
+    let s1 = State1;
+
+    // Create State Machine and State1 as current state
+    let mut mysm = MySm::new(&s1);
 
     // Allocate a message on the stack and dispatch it
     let msg = Protocol1::Msg1 { f1: 123 };
-    mysm.dispatch(&msg);
+
+    // Processes a message
+    mysm.current_state.process(&mut mysm, &msg)
 }
