2d7c9df4bee999505dabb0c8f1e64ff3d2f4cd2f
Wink Saville <wink@saville.com>
2022-10-07 13:10:01 -0700

    refactor: Cleanup
     * Rename MySm to Sm1 and mysm to sm
     * Use Protocol1.f1 in State1::process
     * Clean up some comments
    
    Results:
     $ cargo run
        Compiling expr-traits-1 v0.1.0 (/home/wink/prgs/rust/myrepos/exper-traits-1)
         Finished dev [unoptimized + debuginfo] target(s) in 0.12s
          Running `target/debug/expr-traits-1`
     State1: process sm.data1=124 Msg1::f1=123

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ pub trait State<'a, SM, P> {
 // Create a Protocal with two messages
 struct State1;
 
-impl<'a> State<'a, MySm<'a>, Protocol1> for State1 {
-    fn process(&self, sm: &'a mut MySm, msg: &Protocol1) {
+impl<'a> State<'a, Sm1<'a>, Protocol1> for State1 {
+    fn process(&self, sm: &'a mut Sm1, msg: &Protocol1) {
         match *msg {
             Protocol1::Msg1 { f1 } => {
-                sm.data1 += 1;
+                sm.data1 += f1;
                 println!("State1: process sm.data1={} Msg1::f1={}", sm.data1, f1);
             }
         }
@@ -22,14 +22,14 @@ enum Protocol1 {
     Msg1 { f1: i32 },
 }
 
-struct MySm<'a> {
+struct Sm1<'a> {
     current_state: &'a dyn State<'a, Self, Protocol1>,
     data1: i32,
 }
 
-impl<'a> MySm<'a> {
+impl<'a> Sm1<'a> {
     fn new(s: &'a dyn State<'a, Self, Protocol1>) -> Self {
-        MySm {
+        Sm1 {
             current_state: s,
             data1: 1,
         }
@@ -40,12 +40,12 @@ fn main() {
     // Create State1
     let s1 = State1;
 
-    // Create State Machine and State1 as current state
-    let mut mysm = MySm::new(&s1);
+    // Create State Machine and set State1 as current state
+    let mut sm = Sm1::new(&s1);
 
-    // Allocate a message on the stack and dispatch it
+    // Create a message
     let msg = Protocol1::Msg1 { f1: 123 };
 
     // Processes a message
-    mysm.current_state.process(&mut mysm, &msg)
+    sm.current_state.process(&mut sm, &msg);
 }
